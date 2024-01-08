ef89a6d0c97f85987dd22abc79564516748937fd
MatteoCantafio25 <mattyteo004@gmail.com>
2024-01-08 18:14:25 +0100

    script completed

--- script.js
+++ script.js
@@ -3,3 +3,50 @@
 
 console.log("JS OK");
 
+
+//---------------------------------
+// RECUPERO GLI ELEMENTI DALLA PAGINA
+
+const containerList = document.getElementById("container-list");
+
+//---------------------------------
+// CREO UNA COSTANTE CON L'ARRAY DI PARTENZA
+
+const list = ["farina", "pomodori", "mele", "acqua", "patatine"];
+
+//---------------------------------
+// CREO UN IL TAG DI APERTURA DELL'UL PER AVERE IN PAGINA POI UNA LISTA
+let listItems = "<ul>";
+
+
+//--------------
+// DICHIARO LA I
+i = 0
+while (i < list.length) {
+    // MONTO IL MESSAGGIO CON L'UL
+    listItems += `<li>${list[i]}</li>`;
+
+    // INCREMENTIAMO IL CONTATORE
+    i++
+}
+
+
+//--------------
+// TODO ESEGUO LA STESSA COSA CON UN CICLO FOR
+/*
+for (let i = 0; i < list.length; i++) {
+    listItems += `<li>${list[i]}</li>`;
+}
+*/
+
+//---------------------------------
+// CHIUDO L'UL
+
+listItems += "</ul>";
+
+
+//---------------------------------
+// STAMPO IN PAGINA
+
+containerList.innerHTML += listItems;
+
